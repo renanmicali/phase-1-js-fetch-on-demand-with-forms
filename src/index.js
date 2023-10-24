2e7d732af65a64e75bbe70877e88254743cd258d
Renan Kozuka Micali <renanmicali@hotmail.com>
2023-10-24 12:58:20 +1100

    completed lab/added new ids

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
-  
+    const inputForm = document.querySelector("form");
+
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.querySelector('#searchByID')
+        console.log(input.value)
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+            .then((response) => response.json())
+            .then((data) => {
+                const title = document.querySelector('#title_result')
+                const summary = document.querySelector('#summary_result')
+                
+                
+                title.innerText = data.title
+                summary.innerText = data.summary
+                // LOG: (3) [{…}, {…}, {…}]
+            });
+    })
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
